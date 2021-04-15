60b91f7f7708dd6f1bed69a4a9bfc560a0cfcbe9
Jon Canning <jon.canning@gmail.com>
2021-04-15 17:38:35 +0100

    initial commit

---
+++ Run.fs
@@ -0,0 +1,20 @@
+﻿module Run
+
+open Microsoft.Extensions.Logging
+open Microsoft.Extensions.Configuration
+open Microsoft.Azure.Functions.Worker.Http
+open Microsoft.Azure.Functions.Worker
+open FSharp.Control.Tasks
+
+[<Function "Example">]
+let run ([<HttpTrigger "GET">] req: HttpRequestData, logger: ILogger) =
+    task {
+        let response = req.CreateResponse()
+
+        do!
+            response
+                .WriteAsJsonAsync({| Response = "Hello World!" |})
+                .AsTask()
+
+        return response
+    }
